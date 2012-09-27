f2a473d1dff18304d8c26f38ce7b782200579f88
Pavel Tolstov <ray66rus@gmail.com>
2012-09-27 21:01:03 +0400

    added simple image uploader

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+def full_title(subtitle)
+  base_title = 'Costumes Rent Manager'
+  if subtitle.empty?
+    base_title
+  else
+    "#{base_title} | #{subtitle}"
+  end
+end
+
 end
