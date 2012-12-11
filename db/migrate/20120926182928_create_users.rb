0010ed7b714306bbdb4a8b83ea989abf5e0e180a
Pavel Tolstov <ray66rus@gmail.com>
2012-12-12 00:04:38 +0400

    changed migration scheme

--- db/migrate/20120926182928_create_users.rb
+++ db/migrate/20120926182928_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :name
       t.string :email
       t.string :password
-      t.string :type
+      t.string :user_type
 
       t.timestamps
     end
