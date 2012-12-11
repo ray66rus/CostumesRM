2e24269cd752c51e43c91f1bafe08f5727a22018
Pavel Tolstov <ray66rus@gmail.com>
2012-12-12 00:38:32 +0400

    Work on user sign in and out

--- config/routes.rb
+++ config/routes.rb
@@ -35,5 +35,9 @@ CostumesRM::Application.routes.draw do
 
   resources :users
   match '/signup', to: 'users#new'
-  match :create_user, to: 'users#create'
+  match '/create_user', to: 'users#create'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 end
