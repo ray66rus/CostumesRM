93245dd50009149e52eb0d69d35eeb2eac9a7c75
Pavel Tolstov <ray66rus@gmail.com>
2012-12-29 23:20:23 +0400

    start work on adding tabbed view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,18 @@
 module ApplicationHelper
 
-def full_title(subtitle)
-  base_title = 'Costumes Rent Manager'
-  if subtitle.empty?
-    base_title
-  else
-    "#{base_title} | #{subtitle}"
+  def full_title(subtitle)
+    base_title = 'Costumes Rent Manager'
+    if subtitle.empty?
+      base_title
+    else
+      "#{base_title} | #{subtitle}"
+    end
   end
-end
-
+  
+  def use_tabbed_view?
+    return false if params[:controller] == 'sessions'
+    return false if signed_in? && params[:controller] == 'users' && params[:id].to_s == current_user.id.to_s
+    return true
+  end
 end
 
