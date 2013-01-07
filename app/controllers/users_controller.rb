64bace64613453f14f5661768b496a52264ec0ca
Pavel Tolstov <ray66rus@gmail.com>
2013-01-07 22:58:20 +0400

    added anchors to users in all users list

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
       if current_user.admin?
         last_page = User.count / PAGE_SIZE
         last_page += 1 if last_page * PAGE_SIZE < User.count
-        redirect_to action: :index, page: last_page
+        redirect_to action: :index, page: last_page, anchor: "user_#{@user.id}"
       else
         redirect_to @user
       end
@@ -41,7 +41,7 @@ class UsersController < ApplicationController
         record_number = User.where("id<=" + @user.id.to_s).count
         current_page = record_number / PAGE_SIZE
         current_page += 1 if current_page * PAGE_SIZE < record_number
-        redirect_to action: :index, page: current_page
+        redirect_to action: :index, page: current_page, anchor: "user_#{@user.id}"
       else
         sign_in @user
         redirect_to @user
